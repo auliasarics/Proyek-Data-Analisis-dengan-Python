9771134c8d4bc193f039cd9340e9823a58d41701
auliasarics <132457147+auliasarics@users.noreply.github.com>
2025-01-16 05:13:33 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -44,11 +44,13 @@ sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5)
 plt.title('Heatmap Korelasi antara Parameter Cuaca dan Kualitas Udara')
 st.pyplot(plt)
 
-# Perbandingan kualitas udara antar stasiun
-st.subheader('Perbandingan Kualitas Udara Antar Stasiun (2015-2017)')
+# Perbandingan Kualitas Udara Antar Stasiun
+st.subheader(f'Perbandingan Kualitas Udara Antar Stasiun (2015-2017)')
+
 start_year = 2015
 end_year = 2017
 air_quality_parameters = ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO', 'O3']
+
 station_names = [
     'Aotizhongxin', 'Changping', 'Dingling', 'Dongsi', 'Guanyuan', 'Gucheng', 
     'Huairou', 'Nongzhanguan', 'Shunyi', 'Tiantan', 'Wanliu', 'Wanshouxigong'
@@ -63,14 +65,16 @@ for i, parameter in enumerate(air_quality_parameters):
 
     axs[row, col].set_title(parameter)
     
-    for station in station_names:
-        filtered_data = combined_df[(combined_df['year'] >= start_year) & (combined_df['year'] <= end_year) & (combined_df['station'] == station)]
+    for j, station in enumerate(station_names):
+        filtered_data = combined_df[(combined_df['year'] >= start_year) & 
+                                    (combined_df['year'] <= end_year) & 
+                                    (combined_df['station'] == station)]
         axs[row, col].bar(station, filtered_data[parameter].mean(), label=station)
 
     axs[row, col].set_xlabel('Stasiun')
     axs[row, col].set_ylabel('Rata-rata Kualitas Udara')
+    axs[row, col].legend()
 
 plt.tight_layout(rect=[0, 0, 1, 0.96])
 st.pyplot(fig)
-
 
